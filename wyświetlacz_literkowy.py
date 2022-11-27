5b454f4030c76d939078295894bab87d43d53084
tykfikk <104891802+tykfikk@users.noreply.github.com>
2022-11-27 15:23:40 +0100

    wersja bardziej ostateczna

---
+++ wyświetlacz_literkowy.py
@@ -0,0 +1,59 @@
+import pygame
+import sys
+import time
+import os
+from pygame.locals import (K_ESCAPE, QUIT, KEYDOWN)
+
+pygame.init()
+
+# okno z wyświetlaczem
+szerokosc = 750
+wysokosc = 500
+okno = pygame.display.set_mode([szerokosc,wysokosc])
+pygame.display.set_caption("Wyświetlacz")
+clock = pygame.time.Clock()
+
+# kolory
+czarny = (0, 0, 0)
+bialy = (255, 255, 255)
+
+alfabet = 'ABCDEFGHIJKLMNOPQRSTUWXYZ'
+czcionka = pygame.font.SysFont("Times New Roman", 150)
+
+def wyswieltanie_liter():
+    for litera in alfabet:
+        tekst = czcionka.render(litera, True, bialy)
+        tekst_rect = tekst.get_rect()
+        tekst_rect_center = ((szerokosc/2)-50,(wysokosc/2)-90)
+        tekst_tlo = pygame.draw.rect(okno, czarny, (0, 0, 750, 500))
+        okno.blit(tekst, tekst_rect_center)
+        with open("zapisane_litery.txt", "a") as myfile:
+            myfile.write(litera + ', ' + str(time.time()) + '\n')
+        time.sleep(1)
+        pygame.display.flip()
+
+wyswietlanie = True
+while wyswietlanie:
+
+    pygame.display.update()
+
+    okno.fill(czarny)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            wyswietlanie = False
+
+    wyswieltanie_liter()
+
+    # litera = 'B'
+    # with open("zapisane_litery.txt", "a") as myfile:
+    #     myfile.write(litera + ', ' + str(time.time()) + '\n')
+
+
+pygame.quit()
+            
+
+
+
+
+
